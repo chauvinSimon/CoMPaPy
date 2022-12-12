dd0d9190ea5623fa354bdfc2e1cceb49216cc417
Simon Chauvin <simon.chauvin@esrlabs.com>
2022-12-12 15:52:04 +0100

    feature - save movel plan results

--- scripts/CoMPaPy.py
+++ scripts/CoMPaPy.py
@@ -1,36 +1,41 @@
 #! /usr/bin/env python3
-
 import actionlib
+from datetime import datetime
 import franka_gripper.msg
 import geometry_msgs
-from compapy.scripts.utils import setup_logger, read_yaml
 from geometry_msgs.msg import Pose
-import json
 import moveit_msgs.msg
+from moveit_msgs.msg import RobotTrajectory
 import numpy as np
 from pathlib import Path
+import random
 import rospy
 from sensor_msgs.msg import JointState
 import time
-from typing import Dict, Optional
+from typing import Dict, Optional, Tuple
 
 from moveit_tutorials.doc.move_group_python_interface.scripts.move_group_python_interface_tutorial import \
     MoveGroupPythonInterfaceTutorial
 
-
-def json_load(path: Path):
-    with path.open('r') as f:
-        return json.load(f)
+from compapy.scripts.utils import setup_logger, read_yaml, json_load, pose_to_list, PlanningRes
 
 
 class CoMPaPy(MoveGroupPythonInterfaceTutorial):
-    def __init__(self, log_file: Optional[Path] = None):
+    def __init__(self, log_file: Optional[Path] = None, save_planning_res: bool = False):
         super(CoMPaPy, self).__init__()
 
         if log_file is None:
             saving_dir = Path('logs') / str(time.strftime("%Y%m%d_%H%M%S"))
             saving_dir.mkdir(exist_ok=True, parents=True)
             log_file = saving_dir / 'log.log'
+        else:
+            saving_dir = log_file.parent
+
+        self.planning_res_dir = None
+        if save_planning_res:
+            self.planning_res_dir = saving_dir / 'planning_res'
+            self.planning_res_dir.mkdir(parents=True, exist_ok=True)
+
         self.logger = setup_logger(self.__class__.__name__, log_file=log_file)
 
         self.config = read_yaml(Path('config/compapy.yaml'))
@@ -75,18 +80,33 @@ class CoMPaPy(MoveGroupPythonInterfaceTutorial):
 
         return success
 
-    def move_l(self, target_pose: geometry_msgs.msg.Pose) -> bool:
+    def plan_move_l(
+            self,
+            start_pose: geometry_msgs.msg.Pose,
+            target_pose: geometry_msgs.msg.Pose,
+            resolution_m: Optional[float] = None,
+            jump_threshold: Optional[float] = None,
+    ) -> Tuple[RobotTrajectory, float]:
+
+        if jump_threshold is None:
+            jump_threshold = self.config['move_l']['jump_threshold']
+
+        if resolution_m is None:
+            resolution_m = self.config['move_l']['resolution_m']
+
+        distance = np.linalg.norm([
+            target_pose.position.x - start_pose.position.x,
+            target_pose.position.y - start_pose.position.y,
+            target_pose.position.z - start_pose.position.z,
+        ])
+        self.logger.info(f'want to travel [{distance * 100:.1f} cm]')
+        self.logger.info(f'with resolution_m=[{resolution_m:.5f}] and jump_threshold=[{jump_threshold:.3f}]')
+        self.logger.info(f'... from start_pose =  {pose_to_list(start_pose)}')
+        self.logger.info(f'... to   target_pose = {pose_to_list(target_pose)}')
+
         # do not add current point to the waypoint list
         #   otherwise: "Trajectory message contains waypoints that are not strictly increasing in time."
         #   `https://answers.ros.org/question/253004/moveit-problem-error-trajectory-message-contains-waypoints-that-are-not-strictly-increasing-in-time/`
-        start = self.move_group.get_current_pose().pose
-        distance = np.linalg.norm([
-            target_pose.position.x - start.position.x,
-            target_pose.position.y - start.position.y,
-            target_pose.position.z - start.position.z,
-        ])
-        self.logger.info(f'want to travel [{distance * 100:.1f} cm]')
-
         waypoints = [
             target_pose,
         ]
@@ -95,13 +115,8 @@ class CoMPaPy(MoveGroupPythonInterfaceTutorial):
             speed=self.config['move_l']['speed']
         )
 
-        # todo: tune it
-        #  "The computed trajectory is too short to detect jumps in joint-space Need at least 10 steps,
-        #   only got 3. Try a lower max_step."
-        resolution_m = self.config['move_l']['resolution_m']
-
         # takes as input waypoints of end effector poses, and outputs a joint trajectory that visits each pose
-        (plan, fraction) = self.move_group.compute_cartesian_path(
+        plan, fraction = self.move_group.compute_cartesian_path(
             waypoints=waypoints,
 
             # configurations are computed for every eef_step meters
@@ -115,27 +130,78 @@ class CoMPaPy(MoveGroupPythonInterfaceTutorial):
             # todo: enable jump_threshold (not 0.0), to check for infeasible jumps in joint space
             #  disabling the jump threshold while operating real hardware can cause
             #  large unpredictable motions of redundant joints and could be a safety issue
-            jump_threshold=self.config['move_l']['jump_threshold'],
+            jump_threshold=jump_threshold,
 
-            # avoid_collisions=True  # todo: understand. collision wrt robot or wrt scene-obstacles?
+            # todo: understand. collision wrt robot or wrt scene-obstacles?
+            # avoid_collisions=True
+
+            # todo: add path_constraints?
+            # todo: how to check the current joint-bounds written in `joint_limits.yaml`?
         )
 
-        self.logger.info(f'[{fraction:.1%}] = fraction of the path achieved as described by the waypoints')
-        if fraction == -1.0:
-            self.logger.error('error with compute_cartesian_path')
-            # todo: fallback
+        if self.planning_res_dir is not None:
+            # save planning results (e.g. to check determinism)
+            planning_res = PlanningRes(
+                start_pose=start_pose,
+                target_pose=target_pose,
+                plan=plan,
+                fraction=fraction,
+            )
+            fraction_str = f'{fraction:.0%}'
+            curr_time = datetime.now()
+            timestamp = curr_time.strftime('%Y%m%d_%H%M%S_%f')
+            file_name = f'{timestamp}_cm[{int(distance * 100)}]_f[{fraction_str}].json'
+            planning_res.save(saving_path=self.planning_res_dir / file_name)
 
-        else:
-            n_points = len(plan.joint_trajectory.points)
-            self.logger.info(f'[{n_points}] points (resolution_m = {resolution_m * 100:.1f} cm) -> '
-                             f'path.length at most {n_points * resolution_m * 100:.1f} cm')
-            if fraction < 1.0:
-                self.logger.error('path not complete')
+        return plan, fraction
+
+    def move_l(self, target_pose: geometry_msgs.msg.Pose, n_trials: int = 20) -> bool:
+        start_pose = self.move_group.get_current_pose().pose
+
+        resolution_m = self.config['move_l']['resolution_m']
+        resolution_m_offset = 0
+        jump_threshold = self.config['move_l']['jump_threshold']
+        jump_threshold_offset = 0
+
+        plan = None
+        for i_trial in range(n_trials):
+            if i_trial > 0:
+                self.logger.info(f'trial [{i_trial + 1}] / [{n_trials}]')
+
+            # try different values for (resolution_m, jump_threshold) if the first trial failed
+            if i_trial > 0:
+                resolution_m_offset = random.uniform(-0.002, 0.002)
+                jump_threshold_offset = random.uniform(-1.0, 1.0)
+
+            plan, fraction = self.plan_move_l(
+                start_pose=start_pose,
+                target_pose=target_pose,
+                resolution_m=resolution_m + resolution_m_offset,
+                jump_threshold=jump_threshold + jump_threshold_offset
+            )
+
+            self.logger.info(f'[{fraction:.1%}] = fraction of the path achieved as described by the waypoints')
+
+            if fraction == -1.0:
+                self.logger.error('error with compute_cartesian_path')
                 # todo: fallback
 
-        # Note: we are just planning, not asking move_group to actually move the robot yet
+            else:
+                n_points = len(plan.joint_trajectory.points)
+                self.logger.info(f'[{n_points}] points (resolution_m = {resolution_m * 100:.1f} cm) -> '
+                                 f'path.length at most {n_points * resolution_m * 100:.1f} cm')
+                if fraction == 1.0:
+                    break
+                else:
+                    self.logger.error(f'path not complete [{fraction:.1%}]')
+                    # todo: fallback
 
-        # display a saved trajectory
+        if plan is None:
+            return False
+
+        # we were just planning, not asking move_group to actually move the robot yet
+
+        # display the saved trajectory
         display_trajectory = moveit_msgs.msg.DisplayTrajectory()
         display_trajectory.trajectory_start = self.robot.get_current_state()
         display_trajectory.trajectory.append(plan)
@@ -156,8 +222,18 @@ class CoMPaPy(MoveGroupPythonInterfaceTutorial):
         delta_cm = 100 * np.linalg.norm((target_pose_position - l8_pose_position))
         self.logger.info(f'after [{move_name}]: delta = [{delta_cm:0.2f} cm]')
 
-        target_pose_orientation = np.array([target_pose.orientation.w, target_pose.orientation.x, target_pose.orientation.y, target_pose.orientation.z])
-        l8_pose_orientation = np.array([current_p.orientation.w, current_p.orientation.x, current_p.orientation.y, current_p.orientation.z])
+        target_pose_orientation = np.array([
+            target_pose.orientation.x,
+            target_pose.orientation.y,
+            target_pose.orientation.z,
+            target_pose.orientation.w,
+        ])
+        l8_pose_orientation = np.array([
+            current_p.orientation.x,
+            current_p.orientation.y,
+            current_p.orientation.z,
+            current_p.orientation.w,
+        ])
         delta_q = np.linalg.norm((target_pose_orientation - l8_pose_orientation))
         self.logger.info(f'after [{move_name}]: delta_q = [{delta_q:0.3f}]')
 
