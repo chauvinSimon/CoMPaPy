af15dc8f51c26ecd51fbe5915daec46307ac26b7
Simon Chauvin <simon.chauvin@esrlabs.com>
2022-12-12 15:12:31 +0100

    refactor - enable logging without file-saving

--- scripts/utils.py
+++ scripts/utils.py
@@ -1,39 +1,87 @@
+import json
 import logging
+import os
 from pathlib import Path
+from typing import List, Optional, Dict
 import yaml
+
+from geometry_msgs.msg import Pose
+from geometry_msgs.msg import Point
+from geometry_msgs.msg import Quaternion
 
 formatter = logging.Formatter('%(levelname)s - %(asctime)s - %(module)s: %(message)s')
 
 
-# todo: in pycharm, set format pattern to
+# todo: in pycharm (log highlighting ideolog) set format pattern to
 #   ^(\d{4}-\d{2}-\d{2}\s\d{2}:\d{2}:\d{2},\d{3})\s(\S*)\s*(\w*)\s*(.*)$
+#   note: configure `Grep Console` to format logs into the console
 
 
-def setup_logger(name: str, log_file: Path, level=logging.DEBUG):
+def setup_logger(name: str, log_file: Optional[Path] = None, level=logging.DEBUG):
     print(f'logging [{name}] at [{log_file}]')
-
-    log_file.parent.mkdir(exist_ok=True, parents=True)
-
-    handler = logging.FileHandler(log_file)
-    handler.setFormatter(formatter)
 
     logger = logging.getLogger(name)
     logger.setLevel(level)
-    logger.addHandler(handler)
+
+    if log_file is not None:
+        log_file.parent.mkdir(exist_ok=True, parents=True)
+
+        handler = logging.FileHandler(log_file)
+        handler.setFormatter(formatter)
+        logger.addHandler(handler)
 
     ch = logging.StreamHandler()
-    ch.setLevel(logging.DEBUG)
+    ch.setLevel(level)
     ch.setFormatter(formatter)
     logger.addHandler(ch)
 
     return logger
 
 
-def read_yaml(p: Path):
+def json_load(path: Path) -> Dict:
+    with path.open('r') as f:
+        return json.load(f)
+
+
+def json_dump(obj, path: Path) -> None:
+    with path.open('w') as f:
+        json.dump(obj, f, indent=4)
+
+
+def read_yaml(p: Path) -> Dict:
     if not p.exists():
         raise FileExistsError(f'{p} does not exist')
     with p.open("r") as stream:
         return yaml.safe_load(stream)
+
+
+def list_to_pose(li: List[float]) -> Pose:
+    return Pose(
+        Point(*li[:3]),
+        Quaternion(*li[3:])
+    )
+
+
+def pose_to_list(pose: Pose) -> List[float]:
+    return [
+        pose.position.x,
+        pose.position.y,
+        pose.position.z,
+        pose.orientation.x,
+        pose.orientation.y,
+        pose.orientation.z,
+        pose.orientation.w,
+    ]
+
+
+def get_latest_folder(root_folder: Path, pattern: str = '*/') -> Path:
+    latest_folder = max([
+        p for p in Path(root_folder).glob(pattern)
+        if p.is_dir()
+    ],
+        key=os.path.getmtime
+    )
+    return latest_folder
 
 
 def log_example():
