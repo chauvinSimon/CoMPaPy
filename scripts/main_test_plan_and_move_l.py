c3df66c0a09c06c2de1c2f9c475957d83ac541db
Simon Chauvin <simon.chauvin@esrlabs.com>
2022-12-15 16:30:30 +0100

    feature - plot dist of failed angles

--- scripts/main_test_plan_and_move_l.py
+++ scripts/main_test_plan_and_move_l.py
@@ -1,8 +1,11 @@
+import copy
 import numpy as np
 from scipy.spatial.transform import Rotation
 
 from compapy.scripts.CoMPaPy import CoMPaPy
 from compapy.scripts.utils import list_to_pose, pose_to_list
+
+np.random.seed(0)
 
 
 def plan_or_move_l_in_target_space(
@@ -12,10 +15,12 @@ def plan_or_move_l_in_target_space(
         n_plan_trials: int = 3
 ):
     target_init_pose = [
-        0.5, 0, 0.5,
-        0.923879532, 0.38268343, 0.0, 0.0
+        0.5, 0, 0.6125,
+        0.923879532, -0.38268343, 0.0, 0.0
     ]
     target_init_pose = list_to_pose(target_init_pose)
+    # target_init_pose = compapy.move_group.get_current_pose().pose
+
     success = compapy.move_j(target_pose=target_init_pose)
     if not success:
         raise RuntimeError('cannot reach init pose')
@@ -25,20 +30,21 @@ def plan_or_move_l_in_target_space(
         assert abs(res - expect) < 1e-2, f'{init_pose} \n!= \n{target_init_pose}'
 
     # target space
-    xy_semi_size = 0.03
-    x_min = -0.118 - xy_semi_size
-    x_max = -0.118 + xy_semi_size
+    x_semi_size = 0.15
+    x_min = 0.01 - x_semi_size
+    x_max = 0.01 + x_semi_size
 
-    y_min = -0.636 - xy_semi_size
-    y_max = -0.636 + xy_semi_size
+    y_semi_size = 0.05
+    y_min = -0.65 - y_semi_size
+    y_max = -0.65 + y_semi_size
 
-    z_min = -0.159
-    z_max = -0.159
-    z_above_offset = 0.15
+    l8_to_ee = 0.113
+    z_min = -0.164 + l8_to_ee
+    z_max = -0.164 + l8_to_ee
+    z_above_offset = 0.55
 
-    # todo: why not error when [-160°, 20°] and problems for >20°?
-    rz_deg_min = -160
-    rz_deg_max = 20
+    rz_deg_min = -270
+    rz_deg_max = 10
 
     resolution_m = compapy.config['move_l']['resolution_m']
     resolution_m_offset = 0
@@ -47,6 +53,7 @@ def plan_or_move_l_in_target_space(
 
     failure_msgs = []
     n_success_moves = 0
+    failure_angles = []
 
     for i in range(n_samples):
         x = np.random.uniform(x_min, x_max)
@@ -70,6 +77,21 @@ def plan_or_move_l_in_target_space(
             success_to_target = compapy.move_l(target_pose=target_pose)
             i_str = f'[{i + 1}/{n_samples}]'
             print(f'{i_str} [to-target] [{"success" if success_to_target else "FAIL"}]')
+
+            # down
+            print('trying [down] ...')
+            target_pose_down = copy.deepcopy(target_pose)
+            target_pose_down.position.z -= z_above_offset
+            success_down = compapy.move_l(target_pose=target_pose_down)
+            print(f'{i_str} [down] [{"success" if success_down else "FAIL"}]')
+
+            compapy.close_gripper()
+            compapy.open_gripper()
+
+            # up
+            print('trying [up] ...')
+            success_up = compapy.move_l(target_pose=target_pose)
+            print(f'{i_str} [up] [{"success" if success_up else "FAIL"}]')
 
             # back to start
             print('trying [back-to-start] ...')
@@ -105,12 +127,35 @@ def plan_or_move_l_in_target_space(
                       f'(width = {(max(fractions) - min(fractions)):.3%}) after [{n_plan_trials}] trials' \
                       f'([x={x * 100:.2f} cm] y=[{y * 100:.2f} cm] [z={z * 100:.2f} cm] [rz_deg={rz_deg:.1f} deg])'
                 failure_msgs.append(msg)
+                failure_angles.append(rz_deg)
 
     if not want_to_move:
         if failure_msgs:
             print(f'\n{len(failure_msgs)} failures [{len(failure_msgs) / n_samples:.1%}]:')
             for msg in failure_msgs:
                 print(msg)
+
+            positives = [x for x in failure_angles if x >= 0]
+            negatives = [x for x in failure_angles if x <= 0]
+            bounds_str = f'[{min(failure_angles):.1f}'
+            if negatives:
+                bounds_str += f' {max(negatives):.1f}'
+            if positives:
+                bounds_str += f' {min(positives):.1f}'
+            bounds_str += f' {max(failure_angles):.1f}]'
+            print(f'problematic angles (deg) bounds {bounds_str}: {failure_msgs}')
+            import matplotlib.pyplot as plt
+            data = failure_angles
+            binwidth = 1
+
+            plt.hist(
+                data,
+                bins=np.arange(np.floor(min(data)), np.ceil(max(data) + binwidth), binwidth)
+            )
+            plt.xlim(xmin=rz_deg_min, xmax=rz_deg_max)
+            plt.title(f'{len(failure_angles)} failure_angles (deg) {bounds_str}')
+            plt.show()
+
         else:
             print(f'no problem in [{n_samples}] samples :)')
 
