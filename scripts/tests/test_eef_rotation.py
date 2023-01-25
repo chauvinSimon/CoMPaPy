6c2f517a1c60ea6a0760bc3522278243c285eae8
Simon Chauvin <simon.chauvin@esrlabs.com>
2023-01-25 11:54:24 +0100

    test - use unittest

--- scripts/tests/test_eef_rotation.py
+++ scripts/tests/test_eef_rotation.py
@@ -1,33 +1,37 @@
 import numpy as np
+import unittest
 
 from compapy.scripts.CoMPaPy import CoMPaPy
 
 
-def rotate_eef(
-        a_deg_max: int,
-        n_positive_angles: int,
-        max_error_angle_deg: float = 1.0
-):
-    compapy = CoMPaPy()
+class TestRotateEE(unittest.TestCase):
 
-    a_degs = list(np.linspace(start=0, stop=a_deg_max, num=n_positive_angles)) + [0.]  # back to zero at the end
-    for a_deg in a_degs:
-        for sign in [1, -1]:
-            a_deg *= sign
-            success = compapy.rotate_joint('panda_joint7', np.deg2rad(a_deg))
-            if not success:
-                raise RuntimeError(f'failed to move eef to [{a_deg}] deg')
-            res_deg = np.rad2deg(compapy.get_joints()[6])
-            print(f'target = [{a_deg:.1f}]  actual = [{res_deg:.1f}] (deg)')
-            assert abs(res_deg - a_deg) < max_error_angle_deg, f'{a_deg:.1f} not reached: {res_deg:.1f}'
+    def setUp(self) -> None:
+        self.compapy = CoMPaPy()
 
+    def test_rotate_eef(
+            self,
+            a_deg_max: int = 10,
+            n_positive_angles: int = 5,
+            max_error_angle_deg: float = 1.0
+    ):
 
-def main():
-    rotate_eef(
-        a_deg_max=10,
-        n_positive_angles=5
-    )
+        a_degs = list(np.linspace(start=0, stop=a_deg_max, num=n_positive_angles)) + [0.]  # back to zero at the end
+        for a_deg in a_degs:
+            for sign in [1, -1]:
+                a_deg *= sign
+                success = self.compapy.rotate_joint('panda_joint7', np.deg2rad(a_deg))
+                if not success:
+                    raise RuntimeError(f'failed to move eef to [{a_deg}] deg')
+                res_deg = np.rad2deg(self.compapy.get_joints()[6])
+                print(f'target = [{a_deg:.1f}]  actual = [{res_deg:.1f}] (deg)')
+                self.assertAlmostEqual(
+                    res_deg,
+                    a_deg,
+                    delta=max_error_angle_deg,
+                    msg=f'{a_deg:.1f} not reached: {res_deg:.1f}'
+                ),
 
 
 if __name__ == '__main__':
-    main()
+    unittest.main()
--- scripts/tests/test_ref_actions.py
+++ scripts/tests/test_ref_actions.py
@@ -1,56 +1,128 @@
 import copy
+import numpy as np
+from scipy.spatial.transform import Rotation
+import unittest
+
+from geometry_msgs.msg import Pose
 
 from compapy.scripts.CoMPaPy import CoMPaPy
 
 
-def execute_ref_actions():
-    # todo: test the end state after each execution as a regression test
+class TestRefActions(unittest.TestCase):
 
-    compapy = CoMPaPy()
+    def setUp(self) -> None:
+        self.compapy = CoMPaPy()
+        # todo: move to init pose
 
-    # gripper
-    compapy.open_gripper()
-    compapy.close_gripper()
-    compapy.open_gripper()
-    compapy.close_gripper()
+    def test_open_close(self, n_repetition: int = 2, max_error_mm: float = 1.0):
+        self.compapy.config['close_gripper']['width'] = 0.0
+        self.compapy.config['close_gripper']['epsilon_inner'] = 0.001
+        self.compapy.config['close_gripper']['epsilon_outer'] = 0.001
+        for _ in range(n_repetition):
+            for name in ['open_gripper', 'close_gripper']:
+                success = self.compapy.close_gripper() if name == 'close_gripper' else self.compapy.open_gripper()
+                self.assertTrue(success, msg=f'action [{name}] failed')
 
-    # reach a ref position, stretching up as a giraffe
-    compapy.go_to_joint_state()
+                target_width_mm = 1000 * self.compapy.config[name]['width']
+                actual_width_mm, error_msg = self.compapy.get_gripper_width_mm()
+                print(f'target = [{target_width_mm:.1f}]  actual = [{actual_width_mm:.1f}] (mm)')
+                error_msg = f'[{target_width_mm:.1f}] not reached: {actual_width_mm:.1f}'
+                self.assertAlmostEqual(target_width_mm, actual_width_mm, delta=max_error_mm, msg=error_msg)
 
-    # move inside a virtual cube
-    cube_size = 0.05
-    init_pose = compapy.move_group.get_current_pose().pose
-    targets = []
-    # 1- along the internal "long" diagonal
-    target_1 = copy.deepcopy(init_pose)
-    target_1.position.x += cube_size
-    target_1.position.y += cube_size
-    target_1.position.z += cube_size
-    targets.append(target_1)
+    def test_go_to_joint_state(self, max_error_deg: float = 0.1):
+        """
+        reach a ref position, stretching up as a giraffe
+        """
+        self.compapy.go_to_joint_state()
 
-    # 2- along the external diagonal of one face
-    target_2 = copy.deepcopy(init_pose)
-    target_2.position.y += cube_size
-    targets.append(target_2)
+        tau = 2.0 * np.pi
+        target_joints = [0, -tau / 8, 0, -tau / 4, 0, tau / 6, 0]
+        actual_joints = self.compapy.get_joints()
+        for i, (target_joint, actual_joint) in enumerate(zip(target_joints, actual_joints)):
+            self.assertAlmostEqual(
+                target_joint,
+                actual_joint,
+                delta=np.deg2rad(max_error_deg),
+                msg=f'joint [{i}]: target={np.rad2deg(target_joint):.1f} actual={np.rad2deg(actual_joint):.1f} (deg)'
+            )
 
-    # 3- along the edge of one face, vertically
-    target_3 = copy.deepcopy(init_pose)
-    target_3.position.y += cube_size
-    target_3.position.z += cube_size
-    targets.append(target_3)
+    def test_move_l_in_cube(self, cube_size: float = 0.05):
+        """
+        move inside a virtual cube
+        """
+        init_pose = self.compapy.get_pose()
+        targets = []
 
-    # 4- along the external diagonal of one face, back to the init
-    targets.append(init_pose)
+        # 1- along the internal "long" diagonal
+        target_1 = copy.deepcopy(init_pose)
+        target_1.position.x += cube_size
+        target_1.position.y += cube_size
+        target_1.position.z += cube_size
+        targets.append(target_1)
 
-    for target_pose in targets:
-        compapy.move_l(target_pose)
+        # 2- along the external diagonal of one face
+        target_2 = copy.deepcopy(init_pose)
+        target_2.position.y += cube_size
+        targets.append(target_2)
 
-    # some "not too stretched" pose
-    target_pose = compapy.move_group.get_current_pose().pose
-    target_pose.position.x += 0.3
-    target_pose.position.z -= 0.3
-    compapy.move_l(target_pose)
+        # 3- along the edge of one face, vertically
+        target_3 = copy.deepcopy(init_pose)
+        target_3.position.y += cube_size
+        target_3.position.z += cube_size
+        targets.append(target_3)
+
+        # 4- along the external diagonal of one face, back to the init
+        targets.append(init_pose)
+
+        for target_pose in targets:
+            self.compapy.move_l(target_pose)
+            self.assert_pose(target_pose=target_pose, actual_pose=self.compapy.get_pose())
+
+    def test_up_and_down(self):
+        init_pose = self.compapy.get_pose()
+
+        target_pose = copy.deepcopy(init_pose)
+        target_pose.position.x += 0.3
+        target_pose.position.z -= 0.3
+
+        self.compapy.move_l(target_pose)
+        self.assert_pose(target_pose=target_pose, actual_pose=self.compapy.get_pose())
+
+        self.compapy.move_l(init_pose)
+        self.assert_pose(target_pose=init_pose, actual_pose=self.compapy.get_pose())
+
+    def assert_pose(
+            self,
+            target_pose: Pose,
+            actual_pose: Pose,
+            delta_mm: float = 4.0,
+            delta_deg: float = 1.0
+    ):
+        for i, (target_p, actual_p) in enumerate(zip(
+                [target_pose.position.x, target_pose.position.y, target_pose.position.z],
+                [actual_pose.position.x, actual_pose.position.y, actual_pose.position.z]
+        )):
+            target_p_mm = target_p * 1000
+            actual_p_mm = actual_p * 1000
+            self.assertAlmostEqual(
+                target_p_mm, actual_p_mm,
+                delta=delta_mm,
+                msg=f'pose [{i}]: target_mm={target_p_mm:.1f} actual_mm={actual_p_mm:.1f} (mm)'
+            )
+
+        t_o = target_pose.orientation
+        a_o = actual_pose.orientation
+        target_euler = Rotation.from_quat([t_o.x, t_o.y, t_o.z, t_o.w]).as_euler('xyz', degrees=True)
+        actual_euler = Rotation.from_quat([a_o.x, a_o.y, a_o.z, a_o.w]).as_euler('xyz', degrees=True)
+
+        wrap_180 = lambda x: (x + 180) % (2 * 180) - 180
+        for i, (target_r, actual_r) in enumerate(zip(target_euler, actual_euler)):
+            self.assertAlmostEqual(
+                wrap_180(target_r), wrap_180(actual_r),
+                delta=delta_deg,
+                msg=f'euler [{i}]: target_r={target_r:.1f} actual_r={actual_r:.1f} (deg)'
+            )
 
 
 if __name__ == '__main__':
-    execute_ref_actions()
+    unittest.main()
